05eb0c23306c356ada0fe9573eadc27186417522
DDEI-LLOPEZ <lucholopez91@hotmail.es>
2025-02-17 00:43:46 -0500

    add user list and remove user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '@auth/components/login/login.component';
 import { RegisterComponent } from '@auth/components/register/register.component';
+import { UserListComponent } from './pages/user/templates/user-list/user-list.component';
 //import { NewsCardComponent } from './pages/news/components/news-card/news-card.component';
 
 export const routes: Routes = [
@@ -9,6 +10,10 @@ export const routes: Routes = [
   {
     path: 'news',
     loadChildren: () => import('@news/news.routes').then(m => m.NewsModule)
+  },
+  {
+    path: 'user',
+    component: UserListComponent
   },
   {
     path: 'login',
