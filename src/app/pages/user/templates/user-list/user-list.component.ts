65f4dfee1a7832d326a2836299a699d0b27c138c
DDEI-LLOPEZ <lucholopez91@hotmail.es>
2025-02-17 02:18:11 -0500

    add user list and remove user component

--- src/app/pages/user/templates/user-list/user-list.component.ts
+++ src/app/pages/user/templates/user-list/user-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, Input } from '@angular/core';
 import { User } from '@app/models/User';
+import { UserService } from '@app/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   imports: [],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
+  providers: [UserService]
 })
 export class UserListComponent {
-  @Input() user:User = new User('','', '', '', '','',new Date())
+  @Input() user: any; // Asegúrate de tener esta propieda
+   public users: User[]
+    constructor(private _userService: UserService){
+      this.users = this._userService.getUsers()
+    }
 }
