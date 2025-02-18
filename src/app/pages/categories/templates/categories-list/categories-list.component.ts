6ff5e1374c3bed65e676484952800e97901dacf1
Franz Fernandez <franz.fernandez@doxatalent.com>
2025-02-17 22:54:18 -0500

    all categories y publicity

--- src/app/pages/categories/templates/categories-list/categories-list.component.ts
+++ src/app/pages/categories/templates/categories-list/categories-list.component.ts
@@ -1,11 +1,79 @@
 import { Component } from '@angular/core';
+import { CategorieService } from '@app/services/categories.service';
+import { CategoryCardComponent } from '../../components/category-card/category-card.component';
+import { Categories } from '@app/models/Categories';
+import { CommonModule } from '@angular/common';
+import { SubscriptionService } from '@app/services/subscription.service';
+
 
 @Component({
   selector: 'app-categories-list',
-  imports: [],
+  imports: [CommonModule, CategoryCardComponent],
   templateUrl: './categories-list.component.html',
-  styleUrl: './categories-list.component.css'
+  styleUrl: './categories-list.component.css',
+  providers: [CategorieService, SubscriptionService]
 })
 export class CategoriesListComponent {
+  records_categories: Array<any> = []
+  records_suscription_user:Array<any>=[]
+  public updated_categories: Array<any> = []
+
+
+  public id_user= "375290d0-85ef-42af-b5a3-2a2b1b63bf5a"
+
+  constructor(
+    private categoryService: CategorieService,
+    private  subscriptionService: SubscriptionService
+
+  )
+  {
+      this.categoryService.getCategories().subscribe({
+        next:  (info)  => {
+          this.records_categories = info
+        },
+        error: (error) => console.log("Error: ", error)
+      }
+    )
+    
+    //this.records_suscription_user = this.suscriptionService.getSuscriptionByUser(this.id_user)
+      this.subscriptionService.getSuscriptionByUser(this.id_user).subscribe({
+        next: (info) => {
+          this.records_suscription_user = info
+        }
+      })
+
+    this.updateCategoriesWithSubscription()
+
+  }
+  
+  onCheckboxChange(event: { checked: boolean; categoria: Categories }) {
+    if (event.checked) {
+      this.suscribirCategorias(event.categoria);
+    } else {
+      this.anularCategoria(event.categoria);
+    }
+  }
+
+  suscribirCategorias(var_category:Categories):void{
+    console.log("suscribe: ", var_category.categoria)
+  }
+
+  anularCategoria(var_category:Categories):void{
+    console.log("anular suscripcion de: ", var_category.categoria)
+  }
+
+
+  updateCategoriesWithSubscription(): void {
+    // Creamos un Set con los ID de categorías suscritas para búsquedas rápidas
+    const subscribedIds = new Set(this.records_suscription_user.map(sub => sub.id_categorias));
+
+    // Generamos un nuevo array de categorías con la propiedad "is_suscribed"
+    this.updated_categories = this.records_categories.map(category => ({
+      ...category,
+      is_suscribed: subscribedIds.has(category.id)
+    }));
+
+    console.log(this.updated_categories)
+  }
 
 }
--- src/app/pages/publicity/templates/publicity-list/publicity-list.component.ts
+++ src/app/pages/publicity/templates/publicity-list/publicity-list.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { PublicityCardComponent } from '../../components/publicity-card/publicity-card.component';
+import { PublicityService } from '@app/services/publicity.service';
+import { Publicity } from '@app/models/Publicity';
 
 @Component({
   selector: 'app-publicity-list',
-  imports: [],
+  imports: [PublicityCardComponent],
   templateUrl: './publicity-list.component.html',
-  styleUrl: './publicity-list.component.css'
+  styleUrl: './publicity-list.component.css',
+  providers: [PublicityService]
 })
 export class PublicityListComponent {
+  public record_publicity: Array<any>
 
+  constructor(
+    private publicityService: PublicityService
+  ){
+    this.record_publicity = this.publicityService.getPublicity()
+  }
+
+  public createPublicity(new_publicity: Array<any>):void{
+    console.log("Creando Publicidad2: ", new_publicity)
+  }
+
+  public editPublicity(item: Publicity):void{
+    console.log("Editando Publicidad: ", item.titulo)
+  }
+
+  public deletePublicity(item: Publicity):void{
+    console.log("Eliminando Publicidad: ", item.titulo)
+  }
+
+  // public onFileSelected(image: File):void{
+  //   console.log("Cargando imagen:", image)
+  // }
 }
